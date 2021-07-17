fbe8c8f03fe2fa2ceb8bfb1b9d4ec1d21bad0c49
xucan <15071482539@163.com>
2021-07-17 20:16:06 +0800

    代码调整

--- src/main/java/xyz/wsyzz/candy/controller/UserController.java
+++ src/main/java/xyz/wsyzz/candy/controller/UserController.java
@@ -5,14 +5,16 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import xyz.wsyzz.candy.common.ResultData;
+import xyz.wsyzz.candy.entity.RoleMenu;
 import xyz.wsyzz.candy.entity.User;
+import xyz.wsyzz.candy.mapper.RoleMenuMapper;
 import xyz.wsyzz.candy.service.UserService;
 import xyz.wsyzz.candy.shiro.ShiroEncryptUtils;
 import xyz.wsyzz.candy.util.ResultDataUtils;
+
+import java.util.List;
 
 /**
  * Created by ${XC} on 2019/5/1.
@@ -24,8 +26,11 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleMenuMapper roleMenuMapper;
+
     @PostMapping("add")
-    public Object addUser(User user){
+    public ResultData addUser(User user){
         user.setPassword(ShiroEncryptUtils.encryptPassword(user.getPassword(),user.getUserName()));
         userService.addUser(user);
         return ResultDataUtils.success();
@@ -33,7 +38,7 @@ public class UserController {
 
 
     @PostMapping("login")
-    public Object login(@RequestParam("userName")@ApiParam("用户名") String userName,@ApiParam("密码") @RequestParam("password") String password){
+    public ResultData login(@RequestParam("userName")@ApiParam("用户名") String userName,@ApiParam("密码") @RequestParam("password") String password){
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(userName, password);
         subject.login(usernamePasswordToken);
